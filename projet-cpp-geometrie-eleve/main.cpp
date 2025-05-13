a783eedcb8029e7df890f88c3342b1a9f76d1097
hadilkhelili <hadil.khelili@etu-upsaclay.fr>
2025-05-13 15:24:25 +0200

    Update main.cpp

--- projet-cpp-geometrie-eleve/main.cpp
+++ projet-cpp-geometrie-eleve/main.cpp
@@ -19,11 +19,35 @@ int main() {
 	// Draw the picture by connecting the points
 	draw_picture(points);
 	
-	Point centre(0, 0);
-    	Circle c(10, centre);
-	assert(std::abs(c.area() - 314.159265) < 0.001);
-	assert(std::abs(c.circumference() - 62.831853) < 0.001);
-    	c.draw();
-	
+	bool are_equal(double a, double b, double epsilon = 1e-6) {
+		return std::abs(a - b) < epsilon;
+}
+
+
+    
+    Point centre(0, 0);
+    Circle c(10, centre);
+
+    // aire
+    assert(are_equal(c.area(), 314.159265));
+
+    // circonférence
+    assert(are_equal(c.circumference(), 62.831853));
+
+    // translate
+    c.translate(Point(10, 5));
+    assert(are_equal(c.center.x, 10));
+    assert(are_equal(c.center.y, 5));
+
+    //  resize
+    c.resize(2);
+    assert(are_equal(c.radius, 20));
+    assert(are_equal(c.area(), 1256.63706));
+	// Test égalité
+    Circle c2(20, Point(10, 5));
+    assert(c.equals(c2));
+
+    // Dessin du cercle 
+    c.draw();
 	return 0;
 }
