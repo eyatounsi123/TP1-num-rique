24422bc862b36c681d1764c4038ab1e9bde4fb4d
Aghis27 <aghis.yanat@etu-upsaclay.fr>
2025-05-13 15:54:49 +0200

    Update point.cpp

--- projet-cpp-geometrie-eleve/src/point.cpp
+++ projet-cpp-geometrie-eleve/src/point.cpp
@@ -1,3 +1,8 @@
 #include "point.hpp"
-
+#include <cmath>
+using namespace std;
 Point::Point(double x_val, double y_val) : x(x_val), y(y_val) {}
+double Point::distance(Point B){
+    double dis=sqrt((x-B.x)*(x-B.x)+(y-B.y)*(y-B.y));
+    return dis;
+}
